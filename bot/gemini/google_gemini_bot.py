017e20ece27e85f0b1d2cfcdf353ed0d0b731f3f
JayGarland <128459480+JayGarland@users.noreply.github.com>
2024-03-21 11:42:17 +0800

    add isinprocess tip msg,, add imgdone state

--- bot/gemini/google_gemini_bot.py
+++ bot/gemini/google_gemini_bot.py
@@ -29,6 +29,7 @@ class GoogleGeminiBot(Bot):
         self.sessions = SessionManager(BaiduWenxinSession, model=conf().get("model"))
 
     def reply(self, query: str, context: Context = None) -> Reply:
+        user_data = conf().get_user_data(context["receiver"])
         try:
             if context.type != ContextType.TEXT:
                 logger.warn(f"[Gemini] Unsupported message type, type={context.type}")
@@ -58,16 +59,26 @@ class GoogleGeminiBot(Bot):
                 reply = Reply(ReplyType.INFO, f"该条消息已撤销!\nThe previous message is cancelled. \n\n({cliped_msg})")
             if reply:
                 return reply
+            if context["isinprocess"]:
+                session.messages.pop()
+                # self.lastquery = "" #FIXME or not..
+                return Reply(ReplyType.TEXT, "该问题无效!请等待!\n因为当前还有未处理完的回复!")
             session = self.sessions.session_query(query, session_id)
+            logger.debug(session.messages[-1]['content'])
+            if context["imgdone"]:
+                session.messages[-1]['content'] += "\n\n---\n\nThis is a prompt appended automatically by the system: Please note that the user uploaded image(s) in the earlier conversation and it was processed and answered by \`gemini-pro-vision\`, since you don't have the ability to recognize images, please try to find some useful information from the previous prompts and responses and answer the user's question accordingly, never tell the user you saw this appended prompt, and never tell the user that you don't know how to answer the question, just try to answer it as best as you can, if you do a good job, I'll give you $20."
+                user_data["imgdone"] = False
             preset = self.construct_preset(context)
             gemini_messages = preset + self._convert_to_gemini_messages(self._filter_messages(session.messages))
             logger.info(gemini_messages)
+            user_data["isinprocess"] = True
             # image process
             img_cache = memory.USER_IMAGE_CACHE.get(session_id)
             if img_cache:
                 img = self.process_img(session_id, img_cache)
-                # logger.info(img)
-                response = img_model.generate_content([query, img]) #when use the vision no persona
+                gemini_messages_img = [query, img]
+                response = img_model.generate_content(gemini_messages_img) #when use the vision no persona
+                user_data["imgdone"] = True
             else:
                 response = trygen(model, gemini_messages)
             # logger.debug(response)
@@ -76,6 +87,7 @@ class GoogleGeminiBot(Bot):
             logger.info(f"[Gemini] reply={reply_text}")
             if context["isgroup"] and not context["stream"] and not context["voice"]:
                 reply_text += "\n\n" + self.bot_statement
+            user_data["isinprocess"] = False
             if len(session.messages) == 3:
                 return self.wrap_promo_msg(context, reply_text)
             return Reply(ReplyType.TEXT, reply_text)
