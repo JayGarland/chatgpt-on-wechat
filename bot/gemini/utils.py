8b6131d7b928ce75767436247cc68b580d7da288
JayGarland <128459480+JayGarland@users.noreply.github.com>
2024-03-22 09:25:33 +0800

    remove trygen

--- bot/gemini/utils.py
+++ bot/gemini/utils.py
@@ -1,7 +1,4 @@
 from common.log import logger
-from bot.gemini.llm import genai
-import time
-import traceback
 
 
 def clip_message(text):
@@ -18,13 +15,3 @@ def is_chinese(text):
         if '\u4e00' <= char <= '\u9fff':
             return True
     return False
-
-def trygen(model: genai.GenerativeModel, gemini_messages: list, max_retries = 2, delay = 1):
-    for i in range(max_retries):
-        try:
-            return model.generate_content(gemini_messages)
-        except Exception as e:
-            traceback.print_exc()
-            logger.error(f"Exception occurred: {e}. Retrying...")
-            time.sleep(delay)
-    # raise Exception(f"Try generate content failed after {max_retries} retries.")
